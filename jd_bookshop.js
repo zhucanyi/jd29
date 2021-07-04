2f8f1140ec0002b3ad52c0538ec4440e7ad908a1
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2021-07-04 20:03:05 +0800

    Update jd_bookshop.js

--- jd_bookshop.js
+++ jd_bookshop.js
@@ -29,7 +29,7 @@ let ADD_CART = false
 ADD_CART = $.isNode() ? (process.env.PURCHASE_SHOPS ? process.env.PURCHASE_SHOPS : ADD_CART) : ($.getdata("ADD_CART") ? $.getdata("ADD_CART") : ADD_CART);
 // 加入购物车开关，与东东小窝共享
 
-let inviteCodes = ['']
+let inviteCodes = ['b8548883c56847d1afcee084adf67750']
 
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
