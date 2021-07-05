4ce4af0bde32cad11fbf5a67ae47917a7acdd36c
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2021-07-06 05:19:06 +0800

    Update jd_dreamFactory.js

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -1306,7 +1306,9 @@ async function showMsg() {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: `https://gitee.com/seec/share-codes/raw/master/jd_dreamFactory.json`, 'timeout': 10000}, (err, resp, data) => {
+    $.get({url: "https://gitee.com/seec/share-codes/raw/master/jd_dreamFactory.json",headers:{
+      "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
+    }}, async (err, resp, data) => {
       try {
         if (err) {
           console.log(`${JSON.stringify(err)}`)
