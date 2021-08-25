3f9d409284818118959cfde02758da93b9ede873
inoyna12 <86736950+inoyna12@users.noreply.github.com>
2021-08-25 10:11:00 +0800

    Update jdDreamFactoryShareCodes.js

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -20,7 +20,7 @@ if (process.env.DREAM_FACTORY_SHARE_CODES) {
     shareCodes = process.env.DREAM_FACTORY_SHARE_CODES.split();
   }
 } else if (process.env.DREAM_FACTORY_SHARE_CODES) {
-  console.log(`由于您secret里面未提供助力码，优先内部互助，多余次数助力内置码，请知晓！`)
+  console.log(`您secret里面未提供助力码，优先进行自己账号内互助，然后再给脚本内置的码进行助力，请知晓！`)
 }
 for (let i = 0; i < shareCodes.length; i++) {
   const index = (i + 1 === 1) ? '' : (i + 1);
